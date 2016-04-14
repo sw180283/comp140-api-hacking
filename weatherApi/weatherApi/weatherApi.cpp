c604a0905065fe659f17684790d5c9bc7ff1238f
sw180283 <sw180283@falmouth.ac.uk>
2016-04-14 19:15:24 +0100

    Changed project folder layout

---
+++ weatherApi/weatherApi/weatherApi.cpp
@@ -0,0 +1,13 @@
+// weatherApi.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "weatherApi.h"
+
+//TO DO
+// Get information from OpenWeather JSON
+// Convert JSON into C++ code
+// Allow UI to input
+
+http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=82e200ef6d4a970a6d799061a11ed93c 
+API key = 82e200ef6d4a970a6d799061a11ed93c
