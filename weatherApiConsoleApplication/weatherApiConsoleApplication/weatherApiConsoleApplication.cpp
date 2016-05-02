654c098df3fc372510a4027828936ff25d8457eb
Sam Wills <samwillsacc@gmail.com>
2016-05-02 18:27:27 +0100

    Included some JSON comments

--- weatherApiConsoleApplication/weatherApiConsoleApplication/weatherApiConsoleApplication.cpp
+++ weatherApiConsoleApplication/weatherApiConsoleApplication/weatherApiConsoleApplication.cpp
@@ -10,7 +10,10 @@ using namespace web::http;			// Common HTTP functionality
 using namespace web::http::client;		// HTTP client features
 using namespace concurrency::streams;		// Asynchronous streams
 
-											//http://stackoverflow.com/questions/34960421/looping-through-a-list-of-objects
+/*Tutorial casablanca guidance from:
+https://github.com/Microsoft/cpprestsdk
+http://stackoverflow.com/questions/34960421/looping-through-a-list-of-objects*/
+
 void findWeather()
 {
 	// Get the base for the web search using unicode
@@ -19,9 +22,13 @@ void findWeather()
 	// Start building the URL
 	web::uri_builder builder(U("/data/2.5/weather"));
 
+	// Prompt for user input
 	std::cout << "Enter a location\n";
 
+	// Create a suitable unicode string conversions for user input
 	std::wstring location;
+	
+	// Get user location input
 	std::wcin >> location;
 
 	// Search query by location
@@ -45,19 +52,25 @@ void findWeather()
 		for (size_t i = 0; i < forecastWeatherJSON.size(); i++)
 		{
 			web::json::value forecastDayJSON = forecastWeatherJSON[i];
+			
+			// Look for "main" in the array for JSON data
 			web::json::value mainJSON = forecastDayJSON.at(U("main"));
 			std::string weatherDescription = "";
 			if (!mainJSON.is_null())
 			{
+				// Change the weather description
 				weatherDescription = conversions::to_utf8string(mainJSON.as_string());
 			}
+			
+			// Display the location weather 
 			std::cout << "Weather is " << conversions::to_utf8string(weatherDescription) << std::endl;
 
-			web::json::value MainJson = forecastDayJSON.at(U("main"));
+			//web::json::value MainJson = forecastDayJSON.at(U("main"));
 		}
 	}
 }
 
+// Main function calls weather then pauses system until closed
 int main()
 {
 	findWeather();
